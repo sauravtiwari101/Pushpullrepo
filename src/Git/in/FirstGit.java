953d052fb3754a29c58b8db0ca6009890cccd472
saurav <saurav@LAPTOP-9J5RHVFP>
2023-09-14 00:37:56 +0530

    first commit

---
+++ src/Git/in/FirstGit.java
@@ -0,0 +1,8 @@
+package Git.in;
+
+public class FirstGit {
+	public static void main(String[] args) {
+		System.out.println("hello code is pyshed to github");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author saurav
+ *
+ */
+module GITPROJECT {
+}
